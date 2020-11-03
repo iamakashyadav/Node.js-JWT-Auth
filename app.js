9c28ab521203b9afed9ee4af55555eddcf19a1ab
i_am_akash_ <ay157311@gmail.com>
2020-11-03 07:39:31 +0530

    cookies

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.set('view engine', 'ejs');
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
+
 
 // database connection
 mongoose.connect('mongodb://localhost:27017/smoothie', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
@@ -20,6 +23,28 @@ mongoose.connect('mongodb://localhost:27017/smoothie', { useNewUrlParser: true, 
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+
+// cookies
+app.get('/set-cookies', (req, res) => {
+
+  // res.setHeader('Set-Cookie', 'newUser=true');
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
+
+  res.send('you got the cookies!');
+
+});
+
+app.get('/read-cookies', (req, res) => {
+
+  const cookies = req.cookies;
+  console.log(cookies.newUser);
+
+  res.json(cookies);
+
+});
+
 
 
 // Listen to Port
